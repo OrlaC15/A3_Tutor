4f563a424613bbd9d30b301ecdda5c4bb63dc11c
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-11-17 20:47:16 +0000

    main & easyquestions  updated

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require_relative 'question.rb'
 require_relative 'arithmetic_tutor.rb'
 require_relative 'question_list.rb'
-require_relative 'level1_question.rb'
-
+require_relative 'easy_question.rb'
+require_relative 'player.rb'
 
 data = IO.readlines('tutor.dat')
 player=ArithmeticTutor.new  data[0], data[1].to_i, data[2].to_i
