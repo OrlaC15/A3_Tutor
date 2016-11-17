43daba5e29ed8237aaa9e09a18ece5c0b3425bc8
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-11-17 21:03:33 +0000

    updated

--- question_list.rb
+++ question_list.rb
@@ -1,2 +1,34 @@
+module Calculator
+  def answer
+       result = self.last.x + self.last.y
+  end
+end
+
 class QuestionList
+  include Calculator
+  attr_reader :questions
+
+  def initialize
+    @questions = []
+  end
+
+  def make_questions
+    @questions << EasyQuestion.new
+
+  end
+
+  def each
+    1.upto @questions.size do |i|
+    end
+  end
+
+  def print_questions
+    self.each do |question|
+      question.to_s
+    end
+  end
+
+  def last
+    @questions.last
+  end
 end
