6f52ddb12b262ba247a704971f7f75059cb9d153
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-11-17 20:12:57 +0000

    player updated

--- player.rb
+++ player.rb
@@ -1,19 +1,14 @@
 class Player
-
   def initialize name
     @name = name
-
   end
-
   def play_game
     start_game_msg
     while !is_game_over?
       ask_question
-      get_user_answer
-      check_answer
-        end
-    print_results
+      get_answer
+      is_correct?
+    end
+    results
   end
-
 end
-
