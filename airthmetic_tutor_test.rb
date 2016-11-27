47ff9e6c01c4bd097edd7d2be7d0ed711afb7856
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-11-27 23:01:58 +0000

    testing

--- airthmetic_tutor_test.rb
+++ airthmetic_tutor_test.rb
@@ -17,5 +17,29 @@ class ArithmeticTutorTest<Test::Unit::TestCase
         @player.check_level?
       end
       assert(@player.is_game_over?, 'Game over method not working')
-    end
+     end
+  def test_check_level_when_correct
+    @player.this_answer = :Correct
+    @player.last_answer = :Correct
+    @player.check_level?
+    assert(@player.stage.stage==2, 'Level up not working')
+    assert(@player.last_answer==:Initial, 'last answer has not changed')
+  end
+
+  def test_check_level_when_incorect
+
+    @player.this_answer = :Incorrect
+    @player.last_answer = :Incorrect
+    @player.check_level?
+    assert(@player.stage.stage==1, 'Level down not working')
+    assert(@player.number_of_questions==3, 'not decresing question')
+
+
+  end
+  def test_check_level_when_incorect_and_correct
+    @player.this_answer = :Incorrect
+    @player.last_answer = :Correct
+    @player.check_level?
+    assert(@player.last_answer==:Incorrect, 'last answer has not changed')
+  end
   end
